1976d95b101785bc0a473f4586bdf435c7653423
rajnitrehan27 <rajnitrehan27@gmail.com>
2022-06-23 23:03:20 -0400

    created file

---
+++ ds_day01.py
@@ -0,0 +1,53 @@
+
+
+# 'hello'
+# print('hello')
+
+# message = ('Hello', 'Hey', 'Hi')
+# print(message)
+
+# message = ('Hello', 'Hey', 'Hi')
+# print(message)
+
+# info = '''anything in single
+#  or double quotes is textual data'''
+
+# print(info)
+
+# greeting = 'Hello'
+
+# name = 'john'
+
+# print(greeting + name)
+
+# result = '{},{}.'.format (greeting, name)
+# print(result)
+# result = '{greeting},{name}.'
+# print(result)
+# result = f'{greeting},{name}.'
+# print(result)
+
+num_1 = 350
+num_2 = num_1+1
+
+result = num_1 + num_2
+# print (result)
+
+# digit = 5
+# # sum = (num_1 + digit)
+# print(num_2)
+
+# print(digit)
+# print(sum)
+
+print(f"The sum of {num_1} + {num_2} ={result}") 
+
+
+
+
+
+
+# print (greeting + ',' +  name +'.')
+
+
+
